c53d4af9be97575e3f1e3980efb0835f6fca0144
Avanish Kumar Gupta <125639786+avanishgupta07@users.noreply.github.com>
2023-12-31 22:07:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,33 +2,157 @@ const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
-const getWeather = async(city) => {
-    weather.innerHTML = `<h2> Loading... <h2>`
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+const getWeather = async (city) => {
+    weather.innerHTML = `<h2> Loading ... <h2>`
+    const url = https:/const apikey="YOURAPI";
+    window.addEventListener("load", () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                let lon = position.coords.longitude;
+                let lat = position.coords.latitude;
+                const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${apikey}`;
+
+
+                fetch(url).then((res) => {
+                    return res.json();
+                }).then((data) => {
+                    console.log(data);
+                    console.log(new Date().getTime())
+                    var dat = new Date(data.dt)
+                    console.log(dat.toLocaleString(undefined, 'Asia/Kolkata'))
+                    console.log(new Date().getMinutes())
+                    weatherReport(data);
+                })
+            })
+        }
+    })
+
+
+    function searchByCity() {
+        var place = document.getElementById('input').value;
+        var urlsearch = `http://api.openweathermap.org/data/2.5/weather?q=${place}&` + `appid=${apikey}`;
+
+        fetch(urlsearch).then((res) => {
+            return res.json();
+        }).then((data) => {
+            console.log(data);
+            weatherReport(data);
+        })
+        document.getElementById('input').value = '';
+    }
+
+    function weatherReport(data) {
+
+        var urlcast = `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
+
+        fetch(urlcast).then((res) => {
+            return res.json();
+        }).then((forecast) => {
+            console.log(forecast.city);
+            hourForecast(forecast);
+            dayForecast(forecast)
+
+            console.log(data);
+            document.getElementById('city').innerText = data.name + ', ' + data.sys.country;
+            console.log(data.name, data.sys.country);
+
+            console.log(Math.floor(data.main.temp - 273));
+            document.getElementById('temperature').innerText = Math.floor(data.main.temp - 273) + ' °C';
+
+            document.getElementById('clouds').innerText = data.weather[0].description;
+            console.log(data.weather[0].description)
+
+            let icon1 = data.weather[0].icon;
+            let iconurl = "http://api.openweathermap.org/img/w/" + icon1 + ".png";
+            document.getElementById('img').src = iconurl
+        })
+
+    }
+
+    function hourForecast(forecast) {
+        document.querySelector('.templist').innerHTML = ''
+        for (let i = 0; i < 5; i++) {
+
+            var date = new Date(forecast.list[i].dt * 1000)
+            console.log((date.toLocaleTimeString(undefined, 'Asia/Kolkata')).replace(':00', ''))
+
+            let hourR = document.createElement('div');
+            hourR.setAttribute('class', 'next');
+
+            let div = document.createElement('div');
+            let time = document.createElement('p');
+            time.setAttribute('class', 'time')
+            time.innerText = (date.toLocaleTimeString(undefined, 'Asia/Kolkata')).replace(':00', '');
+
+            let temp = document.createElement('p');
+            temp.innerText = Math.floor((forecast.list[i].main.temp_max - 273)) + ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273)) + ' °C';
+
+            div.appendChild(time)
+            div.appendChild(temp)
+
+            let desc = document.createElement('p');
+            desc.setAttribute('class', 'desc')
+            desc.innerText = forecast.list[i].weather[0].description;
+
+            hourR.appendChild(div);
+            hourR.appendChild(desc)
+            document.querySelector('.templist').appendChild(hourR);
+        }
+    }
+
+    function dayForecast(forecast) {
+        document.querySelector('.weekF').innerHTML = ''
+        for (let i = 8; i < forecast.list.length; i += 8) {
+            console.log(forecast.list[i]);
+            let div = document.createElement('div');
+            div.setAttribute('class', 'dayF');
+
+            let day = document.createElement('p');
+            day.setAttribute('class', 'date')
+            day.innerText = new Date(forecast.list[i].dt * 1000).toDateString(undefined, 'Asia/Kolkata');
+            div.appendChild(day);
+
+            let temp = document.createElement('p');
+            temp.innerText = Math.floor((forecast.list[i].main.temp_max - 273)) + ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273)) + ' °C';
+            div.appendChild(temp)
+
+            let description = document.createElement('p');
+            description.setAttribute('class', 'desc')
+            description.innerText = forecast.list[i].weather[0].description;
+            div.appendChild(description);
+
+            document.querySelector('.weekF').appendChild(div)
+        }
+    } /api.openweathermap.org/data / 2.5 / weather ? q = ${ city }& appid=${ API_KEY }& units=metric;
+
     const response = await fetch(url);
     const data = await response.json()
     return showWeather(data)
 }
 const showWeather = (data) => {
     if (data.cod == "404") {
-        weather.innerHTML = `<h2> City Not Found <h2>`
+        weather.innerHTML = `< h2 > City Not Found</h2 > `;
         return;
     }
     weather.innerHTML = `
-        <div>
-            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        < div >
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"  alt=" ">
         </div>
         <div>
             <h2>${data.main.temp} ℃</h2>
-            <h4> ${data.weather[0].main} </h4>
+            <h4>${data.weather[0].main}</h4>
         </div>
-    `
-}
+    `;
+};
 
 form.addEventListener(
     "submit",
-    function(event) {
-        getWeather(search.value)
+    function (event) {
+        getWeather(search.value);
         event.preventDefault();
     }
-)
+);
+
+window.addEventListener("load",()=> {
+  
+});
